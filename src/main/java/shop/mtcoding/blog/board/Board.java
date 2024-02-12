f10347b2da65ca7e5c19adff064a3bffa33da41f
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-12 22:35:11 +0900

    board 테이블 생성

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,20 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Table(name = "board_tb")
+@Data
+@Entity
+public class Board {
+    @Id // pk설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동증가
+    private String title;
+    private String content;
+
+    private int userId; // 테이블 만들어 질때 user_id
+
+    private LocalDateTime createdAt;
+}
